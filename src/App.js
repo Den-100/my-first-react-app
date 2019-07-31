6904bf3f6a2a4199c898a3dfcf3c9af3c8499a1e
Den-100 <nurdeniz100@hotmail.com>
2019-07-31 16:33:14 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import MyComponents from './components/MyComponents';
+import MyDataComponent from './components/MyDataComponent';
 
 function App() {
   return (
     <div className = "App">
       <MyComponents name = "deniz cayli">this is my children</MyComponents>
       <MyComponents name = "hasan">this is my children</MyComponents>
+      <MyDataComponent></MyDataComponent>
     </div>
   );
 }
